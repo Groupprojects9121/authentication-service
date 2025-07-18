4b890014851be63aaa23f74154615de446e236a2
Aditya Singh <sadityakumar189@gmail.com>
2025-07-18 19:13:22 +0530

    replace existing database to cloud database

--- authapi/admin.py
+++ authapi/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from authapi.models import User
+from authapi.models import PasswordResetOTP
 
+admin.site.register(PasswordResetOTP)
 
 @admin.register(User)
 class UserModelAdminForm(admin.ModelAdmin):
